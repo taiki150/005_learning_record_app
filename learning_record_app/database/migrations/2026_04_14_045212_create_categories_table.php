bea9273eec5002f5a06720dfc420bd36d39e2ff0
Matsuoka0711 <taiki1544.0711@gmail.com>
2026-04-22 07:34:23 +0900

    migration編集 DB接続

--- learning_record_app/database/migrations/2026_04_14_045212_create_categories_table.php
+++ learning_record_app/database/migrations/2026_04_14_045212_create_categories_table.php
@@ -12,10 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('user_id');
-
-            $table->foreign('user_id')
+            $table->uuid('id')->primary();
+            
+            $table->foreignUuid('user_id')
                 ->references('id')
                 ->on('users')
                 ->onDelete('cascade');
