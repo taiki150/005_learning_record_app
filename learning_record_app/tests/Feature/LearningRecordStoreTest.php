6d9ff959d91d8f95a26751922c50ee2a60a4c688
Matsuoka0711 <taiki1544.0711@gmail.com>
2026-08-31 08:30:37 +0900

    テスト準備

---
+++ learning_record_app/tests/Feature/LearningRecordStoreTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LearningRecordStoreTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
