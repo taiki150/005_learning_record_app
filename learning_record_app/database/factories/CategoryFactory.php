9eddd5a3d24c635d91268fb73e5588e4f179b8dd
Matsuoka0711 <taiki1544.0711@gmail.com>
2026-09-04 15:20:49 +0900

    テストコード修正

--- learning_record_app/database/factories/CategoryFactory.php
+++ learning_record_app/database/factories/CategoryFactory.php
@@ -25,15 +25,21 @@ class CategoryFactory extends Factory
             'CI/CD','ユニットテスト','セキュリティ','アーキテクチャ','リファクタリング',
         ];
         // 1番目はメインアカウントとして使用中
-        $user = User::find(2);
+        $user = User::find(1);
         $color_code_array = [
-            '#FFFFFF', '#000000', '#333333', '#F8F9FA', '#0D6EFD', 
+            '#FFFFFF', '#000000', '#333333', '#F8F9FA', '#0D6EFD',
             '#6C757D', '#198754', '#FFC107', '#DC3545', '#6610F2',
         ];
         return [
+            'user_id' => null,
             'name' => fake()->randomElement($category_name_array),
-            'user_id' => $user->id,
-            'color_code' => fake()->randomElement($color_code_array),,
+            'color_code' => fake()->randomElement($color_code_array),
+            'delete_flg' => 1,
         ];
+    }
+
+    public function forUser($user)
+    {
+        return $this->state(['user_id' => $user->id]);
     }
 }
--- learning_record_app/tests/Feature/LearningRecordStoreTest.php
+++ learning_record_app/tests/Feature/LearningRecordStoreTest.php
@@ -4,47 +4,89 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use App\Models\Category;
+use App\Models\LearningRecord;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class LearningRecordStoreTest extends TestCase
 {
-    use RefreshDatabase;
     
     public function test_learning_record_stored()
     {
 
         /**
-         * flg管理
+         * フラグ管理
          *  $user_create_flg true -> テストごとにユーザー作成 | false -> 既存のユーザー使用
          *  $category_create_flg true -> テストごとにカテゴリー作成 | false -> 既存のカテゴリー使用
          */
-
         $user_create_flg = true;
         $category_create_flg = true;
 
+        /***********************************************
+         * 
+         *ここからテストコード 
+         * 
+         */
+
         // テストデータ作成
         $user = $user_create_flg ? User::factory()->create() : User::find(1);
-
         if($category_create_flg){
-            Category::factory()->count(10)->create();
+            $categories = Category::factory()->forUser($user)->count(10)->create();
+
         }
 
         $this->actingAs($user);
+
+        $categoryIds = [
+            (string)$categories[4]->id,
+            (string)$categories[6]->id
+        ];
+
         
-        // Act（実行）
+        // リクエストデータ
         $requestData = [
-            'study_date' => '2024-01-01',
+            'study_date' => '2026-09-01',
             'hours' => 2,
             'minute' => 30,
-            'category_id' => [1, 2],
-            'ratio' => [50, 50],
-            'memo' => 'テスト学習',
+            'category_id' => $categoryIds,
+            'ratio' => [
+                $categoryIds[0] => 20,
+                $categoryIds[1] => 80,
+            ],
+            'memo' => 'テストです。',
         ];
 
         $response = $this->postJson('/api/record', $requestData);
 
-        // APIを呼び出す、または処理を実行
+        $response->assertStatus(201);
+        $response->assertJson(['message' => '登録が完了しました']);
+
+
+        // リクエストデータから合計時間（分）を計算
+        $total_duration = ($requestData['hours']*60) + $requestData['minute'];
+
+        // DBの検証①（親：learning_records）
+        $this->assertDatabaseHas('learning_records', [
+            'user_id' => $user->id,
+            'study_date' => $requestData['study_date'],
+            'total_duration' => $total_duration,
+        ]);
+
+        $learningRecord = LearningRecord::where('user_id', $user->id)
+        ->where('study_date', $requestData['study_date'])
+        ->first();
+
+        // DBの検証②（子：learning_record_details）
+        for ($i=0; $i < count($requestData['category_id']); $i++) { 
+            $this->assertDatabaseHas('learning_record_details', [
+                'learning_record_id' => $learningRecord->id,
+                'category_id' => $requestData['category_id'][$i],
+                'ratio' => $requestData['ratio'][$i],
+                'deuration' => $total_duration * ($requestData['ratio'][$i] / 100),
+            ]);
+        }
+
+
         
         // Assert（検証）
         // 結果が正しいか確認
